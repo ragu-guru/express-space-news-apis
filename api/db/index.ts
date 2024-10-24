59ad50ce615e0e9ba3177073fd220ab763ac014d
ragu-guru <ragug87@gmail.com>
2024-10-24 11:37:41 +0800

    minor fix

--- api/db/index.ts
+++ api/db/index.ts
@@ -1,12 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    // user: 'your_username',       // Replace with your PostgreSQL username
-    // host: 'localhost',            // Replace with your host if needed
-    // database: 'your_database',    // Replace with your database name
-    // password: 'your_password',     // Replace with your database password
-    // port: 5432,                   // Default PostgreSQL port
-    connectionString: "postgres://postgres:admin@localhost:5432/postgres",
+    connectionString: process.env.POSTGRES_URL
 });
 
 export default pool;
